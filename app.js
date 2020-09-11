c1ec9a1c199b60f612c5dc87d0beca0bc1ab17b0
Bayraa <bayraal3344@gmail.com>
2020-09-11 21:16:04 +0800

    about huudas uurchluw

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 //  rgba color choose
-const post = document.getElementsByClassName("post-hover");
-post[1].style.background = " rgba(70, 130, 180, 0.7)";
-post[2].style.background = " rgba(171, 32, 32, 0.7)";
-post[3].style.background = " rgba(103, 58, 183, 0.7)";
-post[4].style.background = " rgba(255, 255, 0, 0.8)";
-post[5].style.background = " rgba(102, 178, 102, 0.7)";
+// const post = document.getElementsByClassName("post-hover");
+// post[1].style.background = " rgba(70, 130, 180, 0.7)";
+// post[2].style.background = " rgba(171, 32, 32, 0.7)";
+// post[3].style.background = " rgba(103, 58, 183, 0.7)";
+// post[4].style.background = " rgba(255, 255, 0, 0.8)";
+// post[5].style.background = " rgba(102, 178, 102, 0.7)";
 
 // smooth
 
